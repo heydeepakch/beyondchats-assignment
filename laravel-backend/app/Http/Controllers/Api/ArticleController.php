3ec9cd597906bdf56543c2f0227017bae856f996
Deepak Choudhary <officialdeepakch@gmail.com>
2025-12-22 01:38:09 +0530

    added simple crud operation

---
+++ laravel-backend/app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Article::orderBy('created_at', 'desc')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return Article::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Article::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $article = Article::findOrFail($id);
+        $article->update($request->all());
+        return $article;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Article::findOrFail($id)->delete();
+        return response()->json(['message' => 'Deleted']);
+    }
+}
