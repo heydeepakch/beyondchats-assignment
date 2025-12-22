fb17cfb4980007ccddb03300bff0bdbe8f35c34f
Deepak Choudhary <officialdeepakch@gmail.com>
2025-12-22 18:30:02 +0530

    stiched index.js logic to use all other helper functions together

--- nodejs-script/src/index.js
+++ nodejs-script/src/index.js
@@ -7,44 +7,52 @@ import publishUpdatedArticle from "./publishUpdatedArticle.js";
 
 async function main() {
   try {
+    console.log("Starting the script...");
+
     const latestOriginalArticle = await fetchLatestArticle();
+
     if (!latestOriginalArticle) {
       console.log("No latest original article found");
+      return;
     }
 
-    // const updatedArticle = await checkUpdatedExists(latestOriginalArticle.id);
+    const updatedArticle = await checkUpdatedExists(latestOriginalArticle.id);
 
-    // if(updatedArticle){
-    //     console.log('Updated article found in DB!');
-    //     return;
-    // }
+    if (updatedArticle) {
+      console.log("Updated article already exists in DB, Exiting script!");
+      return;
+    }
 
     const googleLinks = await searchGoogle(latestOriginalArticle.title);
-    if (googleLinks.length === 0) {
-      console.log("No blog links found on Google!");
+    if (googleLinks.length < 2) {
+      console.log("Not enough blog links found on Google, Exiting script!");
+      return;
     }
-
-    console.log(googleLinks);
 
     const referenceContentArray = [];
     for (const link of googleLinks) {
-        const referenceContent = await scrapeArticle(link);
-        referenceContentArray.push(referenceContent);
+      const referenceContent = await scrapeArticle(link);
+      referenceContentArray.push(referenceContent);
     }
-    // console.log(referenceContentArray);
 
-    const rewrittenArticle = await rewriteWithLLM({ originalContent: latestOriginalArticle.content, referenceContent: referenceContentArray });
-    if (!rewrittenArticle) {
-      console.log("Failed to rewrite article with LLM");
-      return;
-    }
-    console.log("Rewritten Article from LLM:");
-    console.log(rewrittenArticle);
+    const rewrittenArticle = await rewriteWithLLM({
+      originalContent: latestOriginalArticle.content,
+      referenceContent: referenceContentArray,
+    });
 
-    const publishedArticle = await publishUpdatedArticle({ title: latestOriginalArticle.title, content: rewrittenArticle, parent_id: latestOriginalArticle.id });
+    const finalContent = `${rewrittenArticle}
+
+---
+# References
+${googleLinks.map((r, i) => `${i + 1}. [${r}](${r})`).join("\n")}`;
+
+    const publishedArticle = await publishUpdatedArticle({
+      title: latestOriginalArticle.title,
+      content: finalContent,
+      parent_id: latestOriginalArticle.id,
+    });
     console.log("Article published successfully");
     console.log(publishedArticle);
-
   } catch (error) {
     console.error("Error in main function");
     console.error(error.message);
