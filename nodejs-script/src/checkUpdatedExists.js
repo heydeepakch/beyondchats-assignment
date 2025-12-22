94e8a34fb70cf3ec8caaaba324836638924ed8be
Deepak Choudhary <officialdeepakch@gmail.com>
2025-12-22 15:41:12 +0530

    implemented checking if updated article exists to safely exit the pipeline

---
+++ nodejs-script/src/checkUpdatedExists.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config({path: '../.env'});
+const BASE_URL = process.env.LARAVEL_BASE_URL;
+
+export default async function checkUpdatedExists(originalArticleId) {
+  try {
+    const response = await axios.get(`${BASE_URL}/articles/${originalArticleId}/updated`);
+
+    // If updated article exists then Laravel will return the updated article
+    if (response.data) {
+      return true;
+    }
+
+    return false;
+  } catch (error) {
+    console.error('Failed to check if updated article exists');
+    console.error(error.message);
+
+    // Fail-safe: assume updated exists to avoid duplication
+    return true;
+  }
+}
--- nodejs-script/src/index.js
+++ nodejs-script/src/index.js
@@ -1,8 +1,13 @@
-import fetchLatestArticle from "./fetchLatestArticle.js";
+import checkUpdatedExists from "./checkUpdatedExists.js";
 
-async function fetchLatest(){
-    const article = await fetchLatestArticle();
-    console.log(article)
+async function checkIfUpdated(){
+    const article = await checkUpdatedExists(17);
+   
+    if(article){
+        console.log('Updated article exists');
+    }else{
+        console.log('Updated article does not exist');
+    }
 }
 
-fetchLatest()
+checkIfUpdated()
