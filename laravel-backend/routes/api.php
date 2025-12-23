2f8593c82b13bd8d34ccf7d88332b72a3b6da4b3
Deepak Choudhary <officialdeepakch@gmail.com>
2025-12-23 13:04:20 +0530

    added a temporary scrapper route to execute the laravel scrapper to extract oldest blogs

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -2,6 +2,17 @@
 
 use App\Http\Controllers\Api\ArticleController;
 use Illuminate\Support\Facades\Route;
+Route::get('/run-scraper', function () {
+    if (request('key') !== env('SCRAPER_SECRET')) {
+        abort(403);
+    }
+
+    Artisan::call('scrape:oldest-blogs');
+
+    return response()->json([
+        'status' => 'Scraper executed'
+    ]);
+});
 Route::get('articles/latest', [ArticleController::class, 'latest']);
 Route::get('articles/{id}/compare', [ArticleController::class, 'compare']);
 Route::get('articles/{id}/updated', [ArticleController::class, 'updated']);
